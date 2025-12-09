06cfd0d113fc41609090b1c9aa5a0764a0b65727
Jonathan Melnyk Mendes <jonathanmelnykmendes77@gmail.com>
2025-12-09 18:25:25 +0000

    feat: first step completed

--- src/TestProject/TestProject.Console/Calculator.cs
+++ src/TestProject/TestProject.Console/Calculator.cs
@@ -2,5 +2,21 @@
 
 public static class Calculator
 {
-    public static int Calculate(string numbers) => 0;
+    public static int Calculate(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+
+        var splitNumbers = numbers.Split(',');
+        var result = 0;
+
+        foreach (var splitNumber in splitNumbers)
+        {
+            result += int.Parse(splitNumber);
+        }
+
+        return result;
+    }
 }
